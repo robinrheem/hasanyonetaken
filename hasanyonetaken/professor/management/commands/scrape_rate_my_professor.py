990bd3b797dffa8f06241a2a42442a8f5ce29084
Robin Rheem <robinrheem@gmail.com>
2023-10-21 09:56:15 -0700

    Implement scrape rate my professor ids command# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## Date:      Sat Oct 21 09:56:15 2023 -0700## On branch main# Your branch is ahead of 'origin/main' by 2 commits.#   (use "git push" to publish your local commits)## Changes to be committed:#	modified:   professor/management/commands/scrape_rate_my_professor.py## Changes not staged for commit:#	modified:   professor/models.py## Untracked files:#	professor/management/commands/scrape_professor_detail.py#	professor/migrations/0001_initial.py#	professor_ids.csv## ------------------------ >8 ------------------------# Do not modify or remove the line above.# Everything below it will be ignored.diff --git a/hasanyonetaken/professor/management/commands/scrape_rate_my_professor.py b/hasanyonetaken/professor/management/commands/scrape_rate_my_professor.pyindex 60b664b..a7e5377 100644--- a/hasanyonetaken/professor/management/commands/scrape_rate_my_professor.py+++ b/hasanyonetaken/professor/management/commands/scrape_rate_my_professor.py@@ -1,7 +1,45 @@+import time+import csv from django.core.management.base import BaseCommand+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError  class Command(BaseCommand):     help = "Scrape all ratings and comments of each professor"      def handle(self, *args, **options):-        self.stdout.write("Hello World")+        school_id = "1967"  # De Anza College+        url = f"https://www.ratemyprofessors.com/search/professors/{school_id}?q=*"+        professor_ids = set()++        with sync_playwright() as p:+            browser = p.chromium.launch()+            page = browser.new_page()+            page.goto(url, timeout=360*1000)+            try:+                self.stdout.write("Close cookie popup")+                page.locator('"Close"').click()+                self.stdout.write("Clicking 'Show More' button")+                page.locator('"Show More"').click()+                self.stdout.write("Waiting for load ...")+                retry = 0+                while True:+                    time.sleep(2)+                    links = set(page.eval_on_selector_all("a", '''elements => elements.map(a => a.href).filter(href => href.includes("/professor/"))'''))+                    self.stdout.write(f"Professor count: {len(professor_ids)}")+                    with open("professor_ids.csv", "a+") as f:+                        writer = csv.writer(f)+                        for row in links.difference(professor_ids):+                            writer.writerow([row])+                        professor_ids.update(links)+                    if not page.locator('"Show More"').is_visible():+                        self.stdout.write("Cannot locate 'Show More' button")+                        self.stdout.write(f"Retrying ... {retry}")+                        retry += 1+                        if retry > 10:+                            break+                    self.stdout.write("Clicking 'Show More' button")+                    page.locator('"Show More"').click()+            except PlaywrightTimeoutError:+                print("Timeout!")+                browser.close()+            self.stdout.write(f"Loaded {len(professor_ids)} professor ids")

--- hasanyonetaken/professor/management/commands/scrape_rate_my_professor.py
+++ hasanyonetaken/professor/management/commands/scrape_rate_my_professor.py
@@ -1,7 +1,45 @@
+import time
+import csv
 from django.core.management.base import BaseCommand
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
 class Command(BaseCommand):
     help = "Scrape all ratings and comments of each professor"
 
     def handle(self, *args, **options):
-        self.stdout.write("Hello World")
+        school_id = "1967"  # De Anza College
+        url = f"https://www.ratemyprofessors.com/search/professors/{school_id}?q=*"
+        professor_ids = set()
+
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            page.goto(url, timeout=360*1000)
+            try:
+                self.stdout.write("Close cookie popup")
+                page.locator('"Close"').click()
+                self.stdout.write("Clicking 'Show More' button")
+                page.locator('"Show More"').click()
+                self.stdout.write("Waiting for load ...")
+                retry = 0
+                while True:
+                    time.sleep(2)
+                    links = set(page.eval_on_selector_all("a", '''elements => elements.map(a => a.href).filter(href => href.includes("/professor/"))'''))
+                    self.stdout.write(f"Professor count: {len(professor_ids)}")
+                    with open("professor_ids.csv", "a+") as f:
+                        writer = csv.writer(f)
+                        for row in links.difference(professor_ids):
+                            writer.writerow([row])
+                        professor_ids.update(links)
+                    if not page.locator('"Show More"').is_visible():
+                        self.stdout.write("Cannot locate 'Show More' button")
+                        self.stdout.write(f"Retrying ... {retry}")
+                        retry += 1
+                        if retry > 10:
+                            break
+                    self.stdout.write("Clicking 'Show More' button")
+                    page.locator('"Show More"').click()
+            except PlaywrightTimeoutError:
+                print("Timeout!")
+                browser.close()
+            self.stdout.write(f"Loaded {len(professor_ids)} professor ids")
